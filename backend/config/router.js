c6b311c43554cd225b89fb26c12da1191c223d5a
CodeX13 <uhnspark@gmail.com>
2023-03-20 19:27:44 +0000

    lesson 3 dummy database

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,4 +1,5 @@
 import express from "express";
+import workouts from "../db/workouts.js"
 
 /*
 This file contains the routes configuration. 
@@ -9,17 +10,25 @@ The response object represents the information sent by the server
 
 const router = express.Router();
 router.get("/workouts", (req, res) => {
-  res.status(200).json("Hello World");
+  res.status(200).json(workouts.find(workout=>workout._id === req.query._id));
 });
-router.post("/workouts", (req, res) => {
-  res.status(200).json("Workout added successfully");
+router.post("/workouts", ({body}, res) => {
+  workouts.push(body)
+  res.status(200).json(workouts);
 });
 
-router.put("/workouts", (req, res) => {
-  res.status(200).json("Workout edited successfully");
+router.put("/workouts", ({body, query}, res) => {
+  console.log(body)
+  console.log(query)
+  let updated_workout = workouts.find(workout=>workout._id === query._id) // identifying the wokrout to edit
+  updated_workout = body // edit the workouts
+  const updated_workouts = workouts.filter(workout=>workout._id !== query._id)
+
+  res.status(200).json([...updated_workouts, updated_workout]);
 })
 
-router.delete("/workouts", (req, res) => {
+router.delete("/workouts", ({query}, res) => {
+  console.log({query})
   res.status(200).json("Workout deleted successfully");
 })
 export default router;
---
+++ backend/db/workouts.js
@@ -0,0 +1,24 @@
+let workouts = [
+    {
+        _id:"1",
+        name:"Push-Ups",
+        day:"Monday",
+        sets:4,
+        reps:10
+    },
+    {
+        _id:"2",
+        name:"Sit-Ups",
+        day:"Sunday",
+        sets:2,
+        reps:12
+    },
+    {
+        _id:"3",
+        name:"Arm Curl",
+        day:"Tuesday",
+        sets:2,
+        reps:20
+    },
+]
+export default workouts
