31e1cd745c46c7330f1eb3b6e949670f0cc324ee
CodeX13 <uhnspark@gmail.com>
2023-04-24 18:55:07 +0100

    backed completed

--- backend/controllers/WorkoutsController.js
+++ backend/controllers/WorkoutsController.js
@@ -1,12 +1,14 @@
 import { WorkoutsModel } from "../models/WorkoutsModel.js"
 export const getWorkouts = async (req, res)=>{
-    res.status(200).json("All workouts")
+    res.status(200).json(await WorkoutsModel.find(req.query))
 }
 export const addWorkout = async (req, res)=>{
     await WorkoutsModel.create(req.body)
     res.status(200).json("Workouts added")
 }
 export const editWorkout = async (req, res)=>{
+    console.log(req.query)
+    await WorkoutsModel.findOneAndUpdate({_id:req.query._id}, req.body)
     res.status(200).json("Workouts added")
 }
 export const deleteWorkout = async (req, res)=>{
